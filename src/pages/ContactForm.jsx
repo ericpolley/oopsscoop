bb1e97b17d0b18366eecf33c64073024d7cd0824
eric polley <ericpolley@live.com>
2023-02-28 20:45:58 -0800

    Open in new window

--- src/pages/ContactForm.jsx
+++ src/pages/ContactForm.jsx
@@ -5,7 +5,7 @@ export default function ContactForm() {
   return (
 
   <div>
-    <a value="signUpForm" href="javascript:void( window.open( 'https://form.jotform.com/230588222618054', 'blank', 'scrollbars=yes, toolbar=no, width=700, height=500' ) )"
+    <a value="signUpForm" href="https://form.jotform.com/230588222618054" target="_blank" rel="noreferrer" 
           className=" font-medium text-2xl mb-4 mt-0"
           >
         <div className='flex flex-col mt-4 justify-center items-center hover:bg-sky-400 border-4 text-white hover:text-black border-slate-500 p-3 w-[380px] md:w-[440px] rounded-lg bg-sky-600 duration-500 shadow-xl shadow-slate-500 hover:shadow-2xl hover:shadow-slate-500'>
